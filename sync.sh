82fb9730eea8c175d5c63b9aa2fe0fb01d3417aa
Alexandre Marcondes <alex@marcondesian.net>
2018-03-17 21:26:27 -0400

    Add logo images

--- sync.sh
+++ sync.sh
@@ -1,1 +1,1 @@
-aws s3 sync . s3://tds-static --acl=public-read --exclude ".DS_Store" --exclude ".git" --exclude ".git/**" --exclude ".gitignore" --exclude "sync.sh" --dryrun
+aws s3 sync . s3://tds-static --acl=public-read --exclude=".DS_Store" --exclude="**/.DS_Store" --exclude=".git" --exclude=".git/**" --exclude=".gitignore" --exclude="sync.sh"
